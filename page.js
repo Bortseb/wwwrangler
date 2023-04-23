81d361788dbbd1982f03e113b4cd00befe31e7c4
Robert Best <chessscholar@gmail.com>
2023-04-23 12:17:07 -0400

    got everything working again, but don't need tids it seems

--- page.js
+++ page.js
@@ -1,23 +1,19 @@
 (async () => {
   const script = browser.runtime.getURL("./frame.js");
   const frame = await import(script);
-
+  console.log("content script worked, (frame)", frame)
   browser.runtime.onMessage.addListener((msg) => {
-    switch (msg.cmd) {
-      case "create-ghost":
-        console.log("trying to create ghost (page)", msg.page)
+    console.log("content script heard msg= ",msg)
+      switch (msg.cmd) {
+          case "create-ghost":
+              console.log("trying to create ghost (page)", msg.page)
+              setTimeout(() => {
+                  frame.open(msg.page)
+              }, 3000)
 
-        let context = frame.context()
-        console.log("frame context = ", context)
-        //change this to try a few times to speak to the frame plugin and create ghost, if fails, then send alert
-        setTimeout(() => {
-          frame.open(msg.page)
-        }, 3000)
-
-        break;
-      default:
-        console.log("Default case used for (msg) in page.js", msg);
-    }
+              break;
+          default:
+              console.log("Default case used for (msg) in page.js", msg);
+      }
   });
-
 })();
--- popup.js
+++ popup.js
@@ -1,10 +1,21 @@
 document.addEventListener("click", (e) => {
   switch (e.target.id) {
     case "create-ghost":
-      browser.runtime.sendMessage({ cmd: "create-ghost" });
+      browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let url = 'http://robert.wiki.openlearning.cc/view/welcome-visitors'
+        let page = {
+          title: "test",
+          story: [{
+            type: 'paragraph',
+            text: `[${url} ${url}]`
+          }]
+        }
+        browser.tabs.sendMessage(tabs[0].id, { cmd: "create-ghost", page: page })
+      });
+      // browser.tabs.create({url: "http://code.fed.wiki/view/frame-integration-promises"});
       break;
     case "wrangle-page":
-      browser.runtime.sendMessage({ cmd: "wrangle-page" });
+      browser.tabs.create({ url: "http://robert.wiki.openlearning.cc/welcome-visitors.html" });
       break;
     case "clear-data":
       browser.runtime.sendMessage({ cmd: "clear-data" });
