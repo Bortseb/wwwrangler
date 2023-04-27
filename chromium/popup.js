6348b65577986d3ed1d91e4f498c6096d6fffddd
Robert Best <chessscholar@gmail.com>
2023-04-27 01:26:37 -0400

    try this again later

--- chromium/popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("click", (e) => {
-  switch (e.target.id) {
-    case "wrangle-page":
-      browser.runtime.sendMessage({ cmd: "wrangle-page" });
-      break;
-    default:
-      console.log("Default case used for click on popup", e.target.id);
-  }
-});
